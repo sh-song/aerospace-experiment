4a11654794f7c17e8ca0dae3659405adaf2af2d5
sh-song <shsong930@gmail.com>
2022-09-18 15:45:10 +0900

    visualize

---
+++ propulsion/filters.py
@@ -0,0 +1,18 @@
+class LowPassFilter(object):
+    def __init__(self, cut_off_freqency, ts):
+    	# cut_off_freqency: 차단 주파수
+        # ts: 주기
+        
+        self.ts = ts
+        self.cut_off_freqency = cut_off_freqency
+        self.tau = self.get_tau()
+
+        self.prev_data = 0.
+        
+    def get_tau(self):
+        return 1 / (2 * np.pi * self.cut_off_freqency)
+
+    def filter(self, data):
+        val = (self.ts * data + self.tau * self.prev_data) / (self.tau + self.ts)
+        self.prev_data = val
+        return val
--- propulsion/parser.py
+++ propulsion/parser.py
@@ -3,26 +3,55 @@ import pandas as pd
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-rawdata_dir = 'rawdata/'
-filenames = os.listdir(rawdata_dir)
-data = {'2bar':[], '6bar':[], \
-            '10bar':[], '14bar':[]}
+from scipy import signal
+from filters import LowPassFilter as LPF
+def load_data(rawdata_dir):
+        V_TO_BAR = 10.0512
+        V_TO_N = 109.7
 
+        filenames = os.listdir(rawdata_dir)
+        data = {'2bar':[], '6bar':[], \
+                    '10bar':[], '14bar':[]}
 
-for filename in filenames:
-	pressure = filename.split('_')[1]
-	file_dir = rawdata_dir + filename
-	raw = pd.read_csv(file_dir, sep='\t')
-	raw = raw.to_numpy()
-	data[pressure].append(raw)
+        for filename in filenames:
+            treatment = filename.split('_')[1]
+            file_dir = rawdata_dir + filename
+            raw = pd.read_csv(file_dir, sep='\t')
+            raw = raw.to_numpy()
+            # Conver scale 
+            raw[:, 1] = convert_scale(raw[:, 1], V_TO_N)
+            raw[:, 2] = convert_scale(raw[:, 2], V_TO_BAR)
+            data[treatment].append(raw)
 
-for key, cases in data.items():
-	print(key)
-	for it in cases:
-		print(it.shape)
-		plt.plot(it[:,1])
-		plt.plot(it[:,2])
-		plt.show()
-iterations = 3
-pressures = ['2', '6', '10', '14']
-prefix = '10'
+        return data
+def save_plot(fig, prefix, name):
+    fig.savefig('figures/'+prefix+'_'+name+'.png')
+
+def convert_scale(vector, scalefactor):
+    return vector * scalefactor
+
+def visualize(replicates, name):
+    fig, axs = plt.subplots(1,3)
+    fig.set_size_inches(15, 5)
+    fig.suptitle(name, fontsize=16)
+
+    for i, rep in enumerate(replicates):
+        t = rep[:, 0]
+        thrust = rep[:, 1]
+        pressure = rep[:, 2]
+
+        axs[i].plot(t, thrust, t, pressure)
+        axs[i].grid(True)
+        axs[i].set_ylim(-10, 5)
+        axs[i].legend(['thrust[N]', 'pressure[bar]'])
+        #axs[i].set_aspect('equal', 'box')
+        axs[i].set_title(f"rep {i}")
+    #fig.tight_layout()
+    save_plot(fig, 'filtered', name)
+if __name__ == "__main__":
+    data = load_data('rawdata/')
+
+    for treatment, replicates in data.items():
+        print(treatment)
+
+        visualize(replicates, treatment)
